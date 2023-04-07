ac7cd8e5cb42c48c9998b18e21802bccfcca979b
rushikeesh <rushikesh.wayal.ai@ghrcem.raisoni.net>
2023-04-07 14:35:00 +0530

    sum program

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(3553);
+        System.out.println("rushi");
+        System.out.println("give some value");
+        Scanner input=new Scanner(System.in);
+        float mark = input.nextFloat();
+        System.out.println(mark);
+    }
+}
