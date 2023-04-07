5ffdc3cfca578e7b7612a9e08b002cfa0e495b5f
rushikeesh <rushikesh.wayal.ai@ghrcem.raisoni.net>
2023-04-07 16:50:46 +0530

    type casting type convertion automatic type promotion in expression unicode value's

---
+++ src/Type_convertion_and_casting.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Type_convertion_and_casting {
+    public static void main(String[] args) {
+
+       // Scanner input =new Scanner(System.in);
+//System.out.println("Enter the float value");
+
+       // float num=input.nextFloat();
+        //System.out.println("Enter the Int value");
+       // int num1 =input.nextInt();
+       // System.out.println(num);
+       // System.out.println(num1);
+// type casting --> explicite
+       // int num3=(int)(33.99f);
+       // System.out.println(num3);
+
+// automatic type promotion in expression
+       // int a=257;
+        //byte b=(byte)(a); // byte only store upto 256 , here 257 % 256 = 1
+        //System.out.println(b);
+
+       // byte a=40;
+        //byte b=30;
+       // byte c=100;
+     //int d = a*b/c ;
+//System.out.println(d);
+
+        //byte b=50;
+      //  b=b*2;  //wrong ,cause we can not assingn integer to byte without convertion
+ // automatic type convertion //ascii value
+      //  int number='a';
+// System.out.println(number);
+
+ //Java allows unicode value's
+    //System.out.println("नमस्कार");
+//System.out.println("नमस्ते");
+//System.out.println("hello");
+
+//        byte b=42;;
+//        char c='a';
+//        short s=1024;
+//        int i=5000;
+//        float f =34.34f;
+//        double d=0.1223;
+//        double result =(f*b)+(i/c)-(d*s);
+//
+//        System.out.println((f*b)+"   "+(i/c)+"   "+(d*s));
+
+
+
+
+
+
+    }
+
+}
